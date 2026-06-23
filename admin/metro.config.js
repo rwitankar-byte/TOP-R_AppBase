178ed35a8aff794ce7d0f92184956868d310b5a1
Rwitankar <rwitankarpal@gmail.com>
2026-06-23 22:53:41 +0530

    Remove unused admin location resolver

--- admin/metro.config.js
+++ admin/metro.config.js
@@ -1,11 +1,3 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
 
-const config = getDefaultConfig(__dirname);
-
-config.resolver.extraNodeModules = {
-  ...(config.resolver.extraNodeModules || {}),
-  "expo-location": path.resolve(__dirname, "src/services/optionalLocation.js")
-};
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
